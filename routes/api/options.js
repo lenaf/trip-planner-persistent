47ced19e0ea48525b33a3c9698e97559cf90db81
Jessica Blake <jcblake14@gmail.com>
2017-06-28 15:43:00 -0400

    api routes

--- routes/api/options.js
+++ routes/api/options.js
@@ -3,8 +3,16 @@
 
 var Promise = require('bluebird');
 var router = require('express').Router();
-var Hotel = require('../models').Hotel;
-var Restaurant = require('../models').Restaurant;
-var Activity = require('../models').Activity;
+var Hotel = require('../../models').Hotel;
+var Restaurants = require('../../models').Restaurant;
+var Activity = require('../../models').Activity;
 
+// router.get('hotels', function(req, res, next){
+//     console.log("HELLO!");
+//     Hotel.findAll()
+//     .then(function(foundHotels){
+//         res.json(foundHotels);
+//     }).catch(next);
+// });
 
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,32 @@ var Hotel = require('../models').Hotel;
 var Restaurant = require('../models').Restaurant;
 var Activity = require('../models').Activity;
 
+// router.use('/api', require('./api/options'));
+
+router.get('/api/hotels', function(req, res, next){
+    console.log("HELLO!");
+    Hotel.findAll()
+    .then(function(foundHotels){
+        res.json(foundHotels);
+    }).catch(next);
+});
+
+router.get('/api/restaurants', function(req, res, next){
+    console.log("HELLO!");
+    Restaurant.findAll()
+    .then(function(foundRestaurants){
+        res.json(foundRestaurants);
+    }).catch(next);
+});
+
+router.get('/api/activities', function(req, res, next){
+    console.log("HELLO!");
+    Activity.findAll()
+    .then(function(foundActivities){
+        res.json(foundActivities);
+    }).catch(next);
+});
+
 router.get('/', function(req, res, next) {
   Promise.all([
     Hotel.findAll(),
@@ -12,7 +38,7 @@ router.get('/', function(req, res, next) {
   ])
   .spread(function(dbHotels, dbRestaurants, dbActivities) {
     res.render('index', {
-      //edit the model imports using AJAX
+      // edit the model imports using AJAX
       // templateHotels: dbHotels,
       // templateRestaurants: dbRestaurants,
       // templateActivities: dbActivities
